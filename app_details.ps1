4d33d07333c15337e2b936171dee280f2c78b875
Sivasankar <68229944+ss94866@users.noreply.github.com>
2025-07-12 22:49:14 +0530

    Update app_details.ps1

--- app_details.ps1
+++ app_details.ps1
@@ -11,6 +11,8 @@ $credential = new-object system.management.automation.pscredential($clientid, $s
 connect-mggraph -tenantid $tenantid -clientsecretcredential $credential -nowelcome
 
 [datetime]$date = (get-date).adddays(-7)
+
+$date = $date.tostring("yyyy-MM-ddTHH:mm:ssZ")
 $sp = get-mgserviceprincipal -filter "createddatetime ge $date"
 # $sp = get-mgserviceprincipal -filter "displayname eq 'check00'"
 $sp

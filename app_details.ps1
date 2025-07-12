080af94d500b7ba461b215ce7358df33322b5e59
Sivasankar <68229944+ss94866@users.noreply.github.com>
2025-07-12 22:29:35 +0530

    Update app_details.ps1

--- app_details.ps1
+++ app_details.ps1
@@ -4,7 +4,7 @@ $tenantID = "29ae2793-ba69-4a18-9c4e-70ec7fa86650"
 $clientID = "985872de-7585-4c9d-b527-9695f1f19bea"
 $clientsecret = "ONG8Q~5pM_JbjL5ko9m7fO~SdhwuF.KukA53UakQ"
 
-$secureclientsecret = converto-securestring $clientsecret -asplaintext -force
+$secureclientsecret = convertto-securestring $clientsecret -asplaintext -force
 
 $credential = new-object system.management.automation.pscredential($clientid, $secureclientsecret)
 

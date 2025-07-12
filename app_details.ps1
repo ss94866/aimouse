839a4ce5a8588c17eaaa8c8108a34e517ab6b93e
Sivasankar <68229944+ss94866@users.noreply.github.com>
2025-07-12 22:28:34 +0530

    Update app_details.ps1

--- app_details.ps1
+++ app_details.ps1
@@ -4,18 +4,11 @@ $tenantID = "29ae2793-ba69-4a18-9c4e-70ec7fa86650"
 $clientID = "985872de-7585-4c9d-b527-9695f1f19bea"
 $clientsecret = "ONG8Q~5pM_JbjL5ko9m7fO~SdhwuF.KukA53UakQ"
 
-$body = @{
-  client_id = $clientid
-  client_secret = $clientsecret
-  scope = "https://graph.microsoft.com/.default"
-  grant_type = "client_credentials"
-}
+$secureclientsecret = converto-securestring $clientsecret -asplaintext -force
 
-$tokenresponse = invoke-restmethod -method Post -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -contenttype "application/x-www-form-urlencoded" -Body $body
+$credential = new-object system.management.automation.pscredential($clientid, $secureclientsecret)
 
-$accesstoken = $tokenresponse.access_token
-
-connect-mggraph -accesstoken $accesstoken
+connect-mggraph -clientid $clientid -tenantid $tenantid -clientsecret $secureclientsecret
 
 get-mgapplication -filter "displayname eq 'testauto'"
 
